443f0397a6d2be49a83e15ee25a6ec98df06f351
Sergui Morejon <serguinajibm20@gmail.com>
2019-09-09 14:14:58 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, flash, redirect
+from contact_model import Contact
+
+# Flask nos pide que enviemos el nombre del modulo
+app = Flask(__name__)
+app.secret_key = 'YOUR_SECRET_KEY'
+app.debug = True
+
+@app.route(r'/', methods=['GET'])
+def contact_book():
+    contacts = Contact.query().fetch()
+    return render_template('contact_book.html', contacts=contacts)
+
+@app.route(r'/add', methods=['GET', 'POST'])
+def add_contact():
+    if request.form:
+        contact = Contact(name=request.form.get('name'), phone=request.form.get('phone'), email=request.form.get('email'))
+
+        contact.put()
+        flash('! Se anadio el contacto !')
+
+    return render_template('add_contact.html')
+
+@app.route(r'/contacts/<uid>', methods=['GET'])    
+def contact_details(uid):
+    contact = Contact.get_by_id(int(uid))
+
+    if not contact:
+        return redirect('/', code=301)
+
+    return render_template('contact.html', contact=contact)
+
+@app.route(r'/delete', methods=['POST'])
+def delete_contact():
+    contact = Contact.get_by_id(int(request.form.get('uid')))
+    contact.key.delete()
+    return redirect('/contacts/{}'.format(contact.key.id()))
+
+if __name__ == "__main__":
+    app.run()    
